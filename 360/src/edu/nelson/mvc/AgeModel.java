41e740982456aa833958f2855a3ab0e1db7617f8
willnelson <willnelson417@me.com>
2017-10-14 13:57:22 -0700

    MVC v2

--- 360/src/edu/nelson/mvc/AgeModel.java
+++ 360/src/edu/nelson/mvc/AgeModel.java
@@ -1,5 +1,15 @@
 package edu.nelson.mvc;
 
+import java.time.Period;
+
 public class AgeModel {
+	
+	/*The purpose of the Model is to house the functions needed to use the data.*/
+	
+	private int currentAge;
+	
+	public void calculateCurrentAge(LocalDate birthDate, LocalDate currentDate) {
+		currentAge = Period.between(birthDate,  currentDate).getYears();
+	}
 
 }
